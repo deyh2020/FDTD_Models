c9ab14a77012d5b114e636ceab4f22e46043b187
Christian Johnson-Richards (PGR) <b5025250@topsy-login.ncl.ac.uk>
2022-04-06 10:11:59 +0100

    bench# Please enter the commit message for your changes. Lines starting

--- LoadedCapillaries/bin/QuickBench.sh
+++ LoadedCapillaries/bin/QuickBench.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH -t 00:30:00         
-#SBATCH --ntasks=6
+#SBATCH -t 00:15:00         
+#SBATCH --ntasks=2
+#SBATCH --threads-per-core=1
 
-module load meep/1.22.0
+module load meep
 
-srun python3 ../LoadedCapillaries/twoDsolve.py MEEP_6_Quick 0
+srun python3 ../LoadedCapillaries/twoDsolve.py MEEP_4_Quick 0
