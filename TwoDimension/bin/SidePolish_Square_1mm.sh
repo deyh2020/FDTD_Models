5c9dd72e56ca52f16482c4b81f78c7f0becef0d4
Christian Johnson-Richards <chriscrossing@localhost.localdomain>
2022-04-18 21:04:30 +0100

    continue sidepolish sim

---
+++ TwoDimension/bin/SidePolish_Square_1mm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -t 12:00:00          
+#SBATCH --ntasks=48
+
+
+expName="SidePolish_Square1mm"
+
+#now=$(date +"%Y-%m-%d")
+now="2022-04-14"
+workingDir="../data/$now/$expName/"
+mkdir -p $workingDir
+logfile="$workingDir/log.out"
+
+module load meep
+
+srun python3 ../TwoDimension/SidePolishedFibre.py ${workingDir} 40 square > ${logfile}
