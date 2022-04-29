dc738778251c46f0b7474c39f8e34e0a71eb3d0b
Christian <chriscrossing@newhuaweiapstaff-10-56-47-185.ncl.ac.uk>
2022-04-29 12:28:08 +0100

    possibly fine but i've been sorting out data storage and everything is mad

--- TwoDimension/Plotting2D/Plotting2Dfield.py
+++ TwoDimension/Plotting2D/Plotting2Dfield.py
@@ -2,30 +2,57 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pickle
 
-WD = "../data/2022-04-21/SidePolish_Square500um/"
-filename = WD + "Transmission_dft.pkl"
-#filename = "../data/2022-04-20/9umThick/Transmission_dft.pkl"
+WD = "../data/2022-04-19/SidePolish_Square500um/"
+fieldFname = WD + "Transmissiondft.pkl"
+#filename = "../data/2022-04-20/6umThick/Transmission_dft.pkl"
+transFname = WD + "Data.pkl"
 
-with (open(filename, "rb")) as openfile:
-        data = pickle.load(openfile)
+with (open(fieldFname, "rb")) as openfile:
+        field = pickle.load(openfile)
+
+with (open(transFname, "rb")) as openfile:
+        Transmission = pickle.load(openfile)
 
 
-WL = 1000*data['WL']
-(x,y,z,w)=data['Location']
-n = data['RefractiveIndex']
+
+
+
+fig,ax = plt.subplots(2,1,figsize=(20,10))
+
+wl = 1/np.array(Transmission['flux_freqs'])
+
+
+
+tran_flux = np.array(Transmission['tran_flux'])
+norm_tran = np.array(Transmission['norm_tran'])
+
+pwr = tran_flux/norm_tran
+
+ax[0].plot(wl*1000,pwr)
+
+ax[0].set_title("3um Thick Walls",fontsize=16)
+
+ax[0].set_ylabel("Un-normalised PWR",fontsize=16)
+
+WL = 1000*field['WL']
+(x,y,z,w)=field['Location']
+n = field['RefractiveIndex']
+
+ax[0].set_xlim(min(WL),max(WL))
+
 
 n = (n - 1.0)**7
-
-print(np.shape(np.abs(data['matrix'])))
-print(np.shape(n))
 
 nMatrix = np.zeros(shape=(len(n),len(WL)))
 for i in range(0,len(WL)):
         nMatrix[:,i] = n
 
 
-plt.figure(figsize=(20,10))
-plt.imshow(
+ax[1].set_ylabel("Monitor Position Y / um",fontsize=16)
+ax[1].set_xlabel("Wavelength / nm",fontsize=16)
+
+
+ax[1].imshow(
         np.flipud(nMatrix),
         aspect=0.3, 
         #interpolation="none",
@@ -33,8 +60,8 @@ plt.imshow(
         cmap="Greys",
         alpha=1)
 
-plt.imshow(
-        np.abs(data['matrix']),
+ax[1].imshow(
+        np.abs(field['matrix']),
         aspect=0.3, 
         interpolation="none",
         extent=(WL[0],WL[-1],y[0],y[-1]),
@@ -42,4 +69,8 @@ plt.imshow(
         alpha=0.7)
 
 
+
+
+
 plt.savefig(WD+"Fields.pdf",dpi=400)
+plt.show()
