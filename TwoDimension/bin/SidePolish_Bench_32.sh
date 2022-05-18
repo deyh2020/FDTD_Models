aeb5c7b8648738f1786ed73f71c4603878427334
Christian Johnson-Richards (PGR) <b5025250@topsy-login.ncl.ac.uk>
2022-05-18 09:18:35 +0100

    data

---
+++ TwoDimension/bin/SidePolish_Bench_32.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -t 05:00:00          
+#SBATCH --ntasks=32
+
+expName="SidePolish_Bench_32"
+roundTrips="1.0"
+GAP="2000"
+
+
+#now=$(date +"%Y-%m-%d")
+now="2022-04-21"
+workingDir="../data/$now/$expName/"
+mkdir -p $workingDir
+logfile="$workingDir/log.out"
+
+
+module load meep
+
+srun python3 ../TwoDimension/SidePolishedFibre.py ${workingDir} ${roundTrips} ${GAP} square > ${logfile}
